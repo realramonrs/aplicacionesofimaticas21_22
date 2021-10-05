fda848a58c7ae404f3472ecde178d5ea664f305c
RamonRS <celmon1@gmail.com>
2021-10-05 14:47:30 +0200

    Dia5deOctubre

---
+++ Tema2/00_ExplicacionVariables/EjercicioRepaso2.vb
@@ -0,0 +1,28 @@
+﻿Module EjercicioRepaso2
+    Sub Main()
+        'Programa que a partir de las horas trabajadas al mes y de la tasa a la que se
+        'pagan esas horas. Calcula el salario neto y el salario bruto
+        'El salario neto es igual al salario bruto - 15%
+        'Declarar variables
+        Dim horasTrabajadas, tasa, salarioBruto, salarioNeto As Double
+
+        'valores de entrada
+        'Solicitar horas trabajadas
+        Console.WriteLine("Introduzca las horas trabajadas:")
+        horasTrabajadas = Convert.ToDouble(Console.ReadLine())
+
+        'Solicitar la tasa
+
+        Console.WriteLine("A cuánto se paga la hora?")
+        tasa = Convert.ToDouble(Console.ReadLine())
+
+
+        salarioBruto = horasTrabajadas * tasa
+        salarioNeto = salarioBruto - 0.15 * salarioBruto
+
+        'Mostrar resultado por pantalla:
+        Console.WriteLine("Salario bruto: " & salarioBruto)
+        Console.WriteLine("Salario neto: " & salarioNeto)
+        Console.ReadLine()
+    End Sub
+End Module
---
+++ Tema2/00_ExplicacionVariables/LecturaTeclado.vb
@@ -0,0 +1,29 @@
+﻿Module LecturaTeclado
+    Sub Main()
+        'Programa que solicita el dni y la edad de una persona
+        Dim dni As String
+        Dim edad As Integer
+
+        'Solicitar por pantalla el DNI
+        Console.WriteLine("Introduzca su DNI: ")
+        dni = Console.ReadLine()
+
+        Try
+            Console.WriteLine("Introduzca su edad: ")
+            edad = Convert.ToInt32(Console.ReadLine())
+            Console.WriteLine("Sus datos han sido correctamente guardados!")
+            Console.WriteLine("DNI: " & dni)
+            Console.WriteLine("Edad " & edad)
+
+        Catch ex As Exception
+            Console.WriteLine("No ha sido posible guardar la información")
+        End Try
+
+
+
+        Console.ReadLine()
+
+
+
+    End Sub
+End Module
