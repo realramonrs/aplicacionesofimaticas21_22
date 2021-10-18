253e7e79e73d49af9a2e931fe53dc250ce762671
RamonRS <celmon1@gmail.com>
2021-10-18 14:23:51 +0200

    Create Ejercicio8.vb
    Boletin

---
+++ Tema2/Boletin/Ejercicio8.vb
@@ -0,0 +1,18 @@
+﻿Module Ejercicio8
+    Sub Main()
+        Dim p, v, n, t As Double
+        Const R As Double = 0.082
+
+        Console.WriteLine("Introduzca el volumen: ")
+        v = Convert.ToDouble(Console.ReadLine())
+        Console.WriteLine("Introduzca el número de moles: ")
+        n = Convert.ToDouble(Console.ReadLine())
+        Console.WriteLine("Introduzca la temperatura: ")
+        t = Convert.ToDouble(Console.ReadLine())
+
+        p = n * R * t / v
+
+        Console.WriteLine("La presion es de : " & p)
+        Console.ReadLine()
+    End Sub
+End Module
