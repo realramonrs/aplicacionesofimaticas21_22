3d36c5d05fb5f0743cf4ede936447330077a77db
RamonRS <celmon1@gmail.com>
2021-10-26 14:45:34 +0200

    dia  26 Octubre

---
+++ Tema3/IfElseIf_SelectCase/04Ejemplo4SelectCase.vb
@@ -0,0 +1,36 @@
+﻿Module _04Ejemplo4SelectCase
+    Sub Main()
+        'Select Case es equivalente a un if elseIf , se suele utilizar
+        'para evaluar valores concretos de una determinada variable
+
+        Dim opcion As Char
+        Console.WriteLine("Escoja una opción: ")
+        Console.WriteLine("a) Calcular la suma de dos numeros")
+        Console.WriteLine("b) Calcular la raiz de un número ")
+        Console.WriteLine("c) Indicar si un número es par o impar")
+        Console.WriteLine("d) Comprobar si un número es superior a 100")
+
+        opcion = Convert.ToChar(Console.ReadLine())
+
+        'Evaluar variable opcion
+        Select Case opcion
+            Case "a"c, "A"c
+
+                Console.WriteLine("Ha escogido la opción a")
+            Case "b"c
+                Console.WriteLine("Ha escogido la opción b")
+            Case "c"c
+                Console.WriteLine("Ha escogido la opción c")
+            Case "d"c
+                Console.WriteLine("Ha escogido la opción d")
+            Case Else
+                Console.WriteLine("Opción errónea.")
+
+
+        End Select
+
+        Console.ReadLine()
+
+
+    End Sub
+End Module
---
+++ Tema3/IfElseIf_SelectCase/05Ejemplo5SelectCase.vb
@@ -0,0 +1,29 @@
+﻿Module _05Ejemplo5SelectCase
+
+    Sub Main()
+        'Programa que si el usuario escoje un número entre 1 y 10 , hace sonar
+        'el console.beep
+        'Si introduce un número 11 o 13 o 15 manda un mensaje con el himno del celta
+        'En cualquier otro caso indica que hay un error
+
+        Dim opcion As Integer
+        Console.WriteLine("Introduzca una opción")
+        opcion = Convert.ToInt32(Console.ReadLine())
+
+        Select Case opcion
+            Case 1 To 10
+                Console.Beep(25000, 2000)
+            Case 11, 13, 15
+                Console.WriteLine("Hala Celta a demostrar, con orgullo e tradición")
+            Case Else
+                Console.WriteLine("Hay un error.")
+        End Select
+
+        If opcion >= 1 And opcion <= 10 Then
+        ElseIf opcion = 11 Or opcion = 13 Or opcion = 15 Then
+        Else
+
+        End If
+        Console.ReadLine()
+    End Sub
+End Module
