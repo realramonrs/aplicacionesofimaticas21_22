40b5cc8cd44ae6b49a083726a2da9769d7a92dd5
RamonRS <celmon1@gmail.com>
2021-11-09 14:45:02 +0100

    Dia 9 Noviembre

---
+++ Tema4/EjemplosBucles/ContadoresConWhile03.vb
@@ -0,0 +1,50 @@
+﻿Module ContadoresConWhile03
+    Sub Main()
+        'Contar cuantos números múltiplos de 7 y 13 hay entre el 100 y y el 1200
+        Dim contador As Integer
+        Dim i As Integer = 100
+
+        While i <= 1200
+            If i Mod 7 = 0 And i Mod 13 = 0 Then
+                contador += 1
+
+            End If
+            i += 1
+        End While
+
+        Console.WriteLine("Hay " & contador & " multiplos de 7 y 13")
+        Console.WriteLine()
+        'Contar los divisores de un número
+
+        Dim n As Integer = 900
+        Dim contador2 As Integer = 0
+        Dim j As Integer = 1
+        Console.WriteLine("Divisores de : " & n)
+
+        While j <= n
+            If n Mod j = 0 Then
+                contador2 += 1
+                Console.Write(j & " ")
+
+            End If
+            j += 1
+        End While
+        Console.WriteLine("Tiene " & contador2 & " divisores")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    End Sub
+End Module
---
+++ Tema4/EjemplosBucles/ExitWhile04.vb
@@ -0,0 +1,44 @@
+﻿Module ExitWhile04
+    Sub Main()
+        'Programa que detecte el PRIMER número múltiplo de 7 entre dos valores
+        Dim n, m As Integer
+        Console.WriteLine("Intro n:(inferior) ")
+        n = Convert.ToInt32(Console.ReadLine())
+        Console.WriteLine("Intro m: (Superior)")
+        m = Convert.ToInt32(Console.ReadLine())
+
+        Dim i As Integer = n
+
+        While i <= m
+            If i Mod 7 = 0 Then
+                Console.WriteLine(i & " Múltiplo de 7")
+                Exit While
+            End If
+            i += 1
+        End While
+
+        'Adivina un número
+        Dim aleatorio As Random = New Random()
+
+        Dim numero As Integer = aleatorio.Next(1, 16)
+        'El usuario tiene 3 intentos
+
+        Dim intentos As Integer = 0
+        Dim valor As Integer 'Número introducido por el usuario
+
+        While intentos < 3
+            Console.WriteLine("Introduce un valor: ")
+            valor = Convert.ToInt32(Console.ReadLine())
+
+            If valor = numero Then
+                Console.WriteLine("Has acertado!!!!!!!!!")
+                Exit While
+            Else
+                intentos += 1
+            End If
+        End While
+
+        Console.ReadLine()
+
+    End Sub
+End Module
