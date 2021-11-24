d77dfd49c32adf7c5388ac6fb9884cdf6d549904
RamonRS <celmon1@gmail.com>
2021-11-24 09:12:19 +0100

    Dia 24 Noviembre

---
+++ Tema4/RepasoBucles/DoLoopWhile.vb
@@ -0,0 +1,37 @@
+﻿Module DoLoopWhile
+    Sub Main()
+        'Ejemplo 1: Programa que solicita número positivo
+        Dim numero As Integer
+        Do
+            Console.WriteLine("Introduzca un número > 0")
+            numero = Convert.ToInt32(Console.ReadLine())
+        Loop While numero < 0
+
+        'Ejemplo 2: Programa que solicita un número entre 1 y 10
+        Dim numero2 As Integer
+        Do
+
+            Console.WriteLine("Introduzca un número entre 1 y 10")
+            numero2 = Convert.ToInt32(Console.ReadLine())
+        Loop While numero2 < 1 Or numero2 > 10
+
+        'Ejemplo 3: Programa que solicita un número múltiplo de 7 y menor que 350
+
+        Dim numero3 As Integer
+        Do
+            Console.WriteLine("Introduzca un número entre múltipo de 7 y menor que 350")
+            numero3 = Convert.ToInt32(Console.ReadLine())
+        Loop While numero3 Mod 7 <> 0 Or Not numero3 <= 350
+
+        'Ejemplo 4: Programa que solicita un valor que no puede estar entre 1 y 10
+        Dim numero4 As Integer
+
+        Do
+            Console.WriteLine("Introduzca un número fuera del intervalo 1 - 10")
+            numero4 = Convert.ToInt32(Console.ReadLine())
+        Loop While numero4 >= 1 And numero4 <= 10
+
+
+        Console.ReadLine()
+    End Sub
+End Module
---
+++ Tema4/RepasoBucles/FactorialYOtros.vb
@@ -0,0 +1,22 @@
+﻿Module FactorialYOtros
+    Sub Main()
+        Dim n As Integer
+        Dim factorial As Long = 1
+        'factorial = n*(n-1)*(n-2)*...*1 (n = 5) fact = 5*4*3*2*1
+        'suma = 1 + 2 + 3 + 4 + 5 + ... + n
+
+        Console.WriteLine("Introduzca n: ")
+        n = Convert.ToInt32(Console.ReadLine())
+
+        If n = 0 Then
+            factorial = 1
+        Else
+            Dim i As Integer = n
+            While i > 1
+                factorial = factorial * i
+                i = i - 1
+            End While
+        End If
+
+    End Sub
+End Module
