f6adf4685e9141f497255bde904ed9b3df9bf3ad
RamonRS <celmon1@gmail.com>
2021-11-24 10:20:42 +0100

    Dia 24

---
+++ 2ªEvaluacion/Tema5/ExplicacionMatrices/EjemplosAccesosArrays02.vb
@@ -0,0 +1,65 @@
+﻿Module EjemplosAccesosArrays02
+    Sub main()
+        Dim temperaturas(29) As Double
+
+
+        'Programa que solicita una temperatura 
+        'Esta temperatura debe de guardarse en la primera pos que no tenga datos
+        'Necesitamos inicializar todas las posiciones del array a un valor absurdo,
+        ' por ejemplo 500
+        'temperaturas(0) = 500
+        'temperaturas(1) = 500
+        'temperaturas(2) = 500
+
+        Dim i As Integer = 0
+        While i < 30
+            temperaturas(i) = 500
+            i += 1
+        End While
+
+        temperaturas(0) = 15.6
+        temperaturas(1) = 20.4
+        temperaturas(2) = 14.3
+        'Solicitamos temperatura 
+        Console.WriteLine("Introduzca la temperatura del día : " & Date.Today.ToShortDateString)
+        Dim temp As Double = Convert.ToDouble(Console.ReadLine())
+
+        'Recorremos matriz en busca de posición vacía (valor 500)
+        Dim contador As Integer = 3
+        For pos As Integer = 0 To 29 Step 1
+            If temperaturas(pos) = 500 Then
+                temperaturas(pos) = temp
+                contador += 1 'Registramos que hemos guardado una temperatura
+                Exit For
+            End If
+        Next
+
+        'Mostrar temperaturas registradas en Noviembre
+        Console.WriteLine("Temperaturas registradas en el mes " & Date.Today.Month)
+        For pos As Integer = 0 To 29 Step 1
+            If temperaturas(pos) <> 500 Then
+                Console.WriteLine("Día " & pos + 1 & " : " & temperaturas(pos))
+            End If
+        Next
+
+        'Calcular la suma de todas las temperaturas
+
+        Dim sumaTemperaturas As Double = 0
+
+        Dim posicion As Integer = 0
+
+        While posicion < 30
+            If temperaturas(posicion) <> 500 Then
+                sumaTemperaturas = sumaTemperaturas + temperaturas(posicion)
+
+            End If
+            posicion += 1
+        End While
+        'Calcular la media
+        '1º Necesito contar las temperaturas
+        Dim media As Double = sumaTemperaturas / contador
+        Console.WriteLine("La temperatura media del mes es : " & media)
+
+        Console.ReadLine()
+    End Sub
+End Module
---
+++ 2ªEvaluacion/Tema5/ExplicacionMatrices/EjemplosDeclaracion01.vb
@@ -0,0 +1,27 @@
+﻿Module EjemplosDeclaracion01
+
+    Sub Main()
+        'Ejemplo 1 : Array de 10 valores de tipo integer
+        Dim x(9) As Integer
+
+        'Ejemplo 2 : Array de 14 valores de tipo String
+        Dim y(13) As String
+
+        'Guardar un String en la posicion 0
+        y(0) = "Zacarias Blanco"
+        y(1) = "Leandro Puente"
+
+        'Ejemplo 3: Array cuyo tamanho es escogido por el usuario
+        Dim n As Integer = 0
+        Console.WriteLine("Introduzca el tamaño de la matriz")
+        n = Convert.ToInt32(Console.ReadLine())
+
+        Dim array(n - 1) As Integer
+
+
+
+
+
+    End Sub
+
+End Module
