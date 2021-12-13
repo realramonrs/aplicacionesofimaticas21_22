5b046abacbd7e28c6f11f96a67acc3628215b3fe
RamonRS <celmon1@gmail.com>
2021-12-13 13:01:42 +0100

    Clase 13 Diciembre

--- 2ªEvaluacion/Tema5/BoletinGuiaArrays/Module1.vb
+++ 2ªEvaluacion/Tema5/BoletinGuiaArrays/Module1.vb
@@ -126,6 +126,62 @@
         Console.WriteLine("Ejercicio 13: ")
         Console.WriteLine("La suma de las posiciones pares es: " & suma13)
 
+        'Ejercicio 14
+        Dim a14(99) As Integer
+        Dim gen As New Random
+        Dim suma14 As Integer = 0
+        'Llenamos con números aleatorios entre 1 y 50
+        For i As Integer = 0 To a14.Length - 1 Step 1
+            a14(i) = gen.Next(1, 51)
+        Next
+
+        For i As Integer = 0 To a14.Length - 1 Step 1
+            suma14 = suma14 + Convert.ToInt32(Math.Pow(a14(i), 2))
+        Next
+
+        'Ejercicio 15
+        Dim a15() = {1, 3, 4, -5, -6, -9, 2, 6}
+        'Sumar los números positivos
+        Dim suma15 As Integer
+
+        For i As Integer = 0 To a15.Length - 1 Step 1
+            If a15(i) > 0 Then
+                suma15 = suma15 + a15(i)
+            End If
+        Next
+        Console.WriteLine("La suma de los cuadrados del array es : " & suma14)
+        Console.ReadLine()
+        'Ejercicio 16
+        Dim a16() As Integer = {4, 5, -2, -8, 9}
+
+        Dim contador16 As Integer
+
+        For i As Integer = 0 To a16.Length - 1 Step 1
+            If a16(i) < 0 Then
+                contador16 += 1
+            End If
+        Next
+
+        Console.WriteLine("Hay " & contador16 & " números negativos")
+        Console.ReadLine()
+        'Ejercicio 17
+        'Porcentaje de números impares
+        Dim a17(51) As Integer
+        llenarMatriz(a17, 1, 100)
+
+        Dim contador17 As Integer
+        Dim porcentaje As Double
+
+        For i As Integer = 0 To a17.Length - 1 Step 1
+            If a17(i) Mod 2 <> 0 Then
+                contador17 += 1
+            End If
+        Next
+
+        porcentaje = (contador17 / a17.Length) * 100
+        Console.WriteLine("Hay un " & Math.Round(porcentaje, 2) & "% de numeros impares")
+        Console.ReadLine()
+
 
         'Ejercicio 18
         Dim a18(100) As String
@@ -141,6 +197,31 @@
             End If
         Next
 
+        'Ejercicio 19
+        Dim a19() As Integer = {200, 50, 900, 60, 400, 51}
+        'Algoritmo que muestra por pantalla los valores > 1000
+        Console.WriteLine("Valores > 1000 : ")
+        For i As Integer = 0 To a19.Length - 1 Step 1
+            If a19(i) > 1000 Then
+                Console.Write(a19(i) & " ")
+            End If
+        Next
+        'Ejercicio 20 con el array anterior
+        'Mostrar solo el primer valor > 1000 junto con la posicion
+        Console.WriteLine()
+        Dim encontrado As Boolean = False
+        Console.WriteLine("Ejercicio 20: Primer valor > 1000 y su posición")
+        For i As Integer = 0 To a19.Length - 1 Step 1
+            If a19(i) > 1000 Then
+                encontrado = True
+                Console.Write(a19(i) & " posicion: " & i)
+                Exit For
+            End If
+        Next
+
+        If Not encontrado Then
+            Console.WriteLine("No hay valores > 1000")
+        End If
 
 
         Console.ReadLine()
