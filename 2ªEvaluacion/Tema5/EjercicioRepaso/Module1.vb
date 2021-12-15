ca3c397af13a394611056a35ebc44092472cb3ae
RamonRS <celmon1@gmail.com>
2021-12-15 09:54:06 +0100

    H

---
+++ 2ªEvaluacion/Tema5/EjercicioRepaso/Module1.vb
@@ -0,0 +1,23 @@
+﻿Module Module1
+
+    Sub Main()
+        'Crear un array con capacidad para n valores de tipo Integer
+        'Siendo n un dato introducido por el usuario (>12)
+        'A continuación realizar los siguientes operaciones:
+        '1. Llenar todas las posiciones con un número negativo
+        '2. Guardar en las 5 primeras posiciones del array y las 5 últimas con números aleatorios
+        '3. El resto de posiciones actualizarlas al número que se os ocurra
+        '4. Mostrar por pantalla el valor de la primera y de la última posición
+        '5. Crear un array copia del array generado.
+        '6. Ordenar el array copia de menor a mayor.
+        '7. Calcular la suma de los valores almacenados en las 10 primeras posiciones de ambos arrays.
+        '8. Sumarle 10 a todas las posiciones del array copia.
+        '9. Poner a cero todas aquellas posiciones que almacenen un´número mayor que la media
+        'en ambos arrays.
+        '10. Solicitar al usuario un valor y buscarlo en ambos arrays , 
+        ' mostrar por pantalla la posición que ocupa. En caso de no encontrarlo avisa de que no 
+        'está en la matriz
+
+    End Sub
+
+End Module
---
+++ 2ªEvaluacion/Tema5/EjercicioRepaso/My Project/AssemblyInfo.vb
@@ -0,0 +1,35 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' La información general de un ensamblado se controla mediante el siguiente 
+' conjunto de atributos. Cambie estos valores de atributo para modificar la información
+' asociada con un ensamblado.
+
+' Revisar los valores de los atributos del ensamblado
+
+<Assembly: AssemblyTitle("EjercicioRepaso")>
+<Assembly: AssemblyDescription("")>
+<Assembly: AssemblyCompany("")>
+<Assembly: AssemblyProduct("EjercicioRepaso")>
+<Assembly: AssemblyCopyright("Copyright ©  2021")>
+<Assembly: AssemblyTrademark("")>
+
+<Assembly: ComVisible(False)>
+
+'El siguiente GUID sirve como id. de typelib si este proyecto se expone a COM.
+<Assembly: Guid("dce7afe0-1804-4ba6-914f-bdfe44870c3a")>
+
+' La información de versión de un ensamblado consta de los cuatro valores siguientes:
+'
+'      Versión principal
+'      Versión secundaria
+'      Número de compilación
+'      Revisión
+'
+' Puede especificar todos los valores o usar los valores predeterminados de número de compilación y de revisión
+' utilizando el carácter "*", como se muestra a continuación:
+' <Assembly: AssemblyVersion("1.0.*")>
+
+<Assembly: AssemblyVersion("1.0.0.0")>
+<Assembly: AssemblyFileVersion("1.0.0.0")>
