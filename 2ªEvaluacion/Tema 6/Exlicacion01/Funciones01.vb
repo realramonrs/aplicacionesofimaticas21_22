ec0da88d8cce3ce790fcbea11565d3789ee89349
RamonRS <celmon1@gmail.com>
2022-01-25 09:15:28 +0100

    Funciones

---
+++ 2ªEvaluacion/Tema 6/Exlicacion01/Funciones01.vb
@@ -0,0 +1,42 @@
+﻿Module Funciones01
+    'Función que calcula la raíz cuadrada de dos números enteros
+    'En caso de no poder hallarla devuelve cero.
+    Public Function raizSuma2(ByRef n1 As Integer, ByVal n2 As Integer) As Double
+        n1 = 2000
+        Dim suma As Integer = n1 + n2
+        Dim raiz As Double = 0
+        If suma >= 0 Then
+            raiz = Math.Sqrt(suma)
+
+        End If
+
+        Return raiz
+
+    End Function
+
+    Public Function sumaArray(ByRef m() As Integer) As Integer
+        Dim suma As Integer = 0
+
+        For Each valor As Integer In m
+            suma += valor
+        Next
+
+        Return suma
+
+    End Function
+
+    Public Sub e(ByVal mensaje As String)
+        Console.WriteLine(mensaje)
+
+    End Sub
+
+    'Procedimiento que muestra un array de enteros por pantalle
+    Public Sub mostrarArray(ByRef m() As Integer)
+        For Each valor As Integer In m
+            Console.Write(valor & " ")
+
+        Next
+        Console.WriteLine()
+    End Sub
+
+End Module
---
+++ 2ªEvaluacion/Tema 6/Exlicacion01/Module1.vb
@@ -0,0 +1,39 @@
+﻿Module Module1
+
+    Sub Main()
+
+        'Probar la función e:
+        e("Bienvenidos al maravilloso mundo de las funciones!!!!!")
+
+        'Probar la función raizSuma2
+        Dim x1 As Integer = 10
+        Dim x2 As Integer = -1
+        'Calcular la raiz de la suma
+        Dim raiz As Double = raizSuma2(x1, x2)
+        e("X1 = " & x1)
+        Dim raiz2 As Double = raizSuma2(9, 4)
+
+        e("Raiz 1: " & raiz)
+        e("Raiz 2: " & raiz2)
+
+
+        'Prueba de la función sumaArray
+
+        Dim matriz() As Integer = {1, 2, 3, 4, 5, 6}
+
+        Dim suma As Integer = sumaArray(matriz)
+        e("La suma de la matriz es: " & suma)
+
+
+        'Prueba de la función mostrarArray:
+        Dim matriz2() As Integer = {1, 2, 3, 4, 5, 6}
+        e("Prueba de la función mostrar Array")
+        mostrarArray(matriz2)
+
+
+
+        Console.ReadLine()
+
+    End Sub
+
+End Module
