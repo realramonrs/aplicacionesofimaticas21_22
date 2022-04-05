8c5ba2b48f382db51992a0df918305d527540479
RamonRS <celmon1@gmail.com>
2022-04-05 14:46:36 +0200

    Dia 5

---
+++ 3ª Evaluacion/Repaso3ConexionFicheros/Form1.Designer.vb
@@ -0,0 +1,118 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form reemplaza a Dispose para limpiar la lista de componentes.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requerido por el Diseñador de Windows Forms
+    Private components As System.ComponentModel.IContainer
+
+    'NOTA: el Diseñador de Windows Forms necesita el siguiente procedimiento
+    'Se puede modificar usando el Diseñador de Windows Forms.  
+    'No lo modifique con el editor de código.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.btnGuardarCancion = New System.Windows.Forms.Button()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.txtTitulo = New System.Windows.Forms.TextBox()
+        Me.txtGrupo = New System.Windows.Forms.TextBox()
+        Me.ListBox1 = New System.Windows.Forms.ListBox()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'btnGuardarCancion
+        '
+        Me.btnGuardarCancion.Location = New System.Drawing.Point(96, 155)
+        Me.btnGuardarCancion.Name = "btnGuardarCancion"
+        Me.btnGuardarCancion.Size = New System.Drawing.Size(174, 23)
+        Me.btnGuardarCancion.TabIndex = 0
+        Me.btnGuardarCancion.Text = "Guardar Canción"
+        Me.btnGuardarCancion.UseVisualStyleBackColor = True
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(93, 52)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(33, 13)
+        Me.Label1.TabIndex = 1
+        Me.Label1.Text = "Titulo"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(93, 99)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(66, 13)
+        Me.Label2.TabIndex = 2
+        Me.Label2.Text = "Grupo/Autor"
+        '
+        'txtTitulo
+        '
+        Me.txtTitulo.Location = New System.Drawing.Point(170, 52)
+        Me.txtTitulo.Name = "txtTitulo"
+        Me.txtTitulo.Size = New System.Drawing.Size(100, 20)
+        Me.txtTitulo.TabIndex = 3
+        '
+        'txtGrupo
+        '
+        Me.txtGrupo.Location = New System.Drawing.Point(170, 96)
+        Me.txtGrupo.Name = "txtGrupo"
+        Me.txtGrupo.Size = New System.Drawing.Size(100, 20)
+        Me.txtGrupo.TabIndex = 4
+        '
+        'ListBox1
+        '
+        Me.ListBox1.FormattingEnabled = True
+        Me.ListBox1.Location = New System.Drawing.Point(493, 60)
+        Me.ListBox1.Name = "ListBox1"
+        Me.ListBox1.Size = New System.Drawing.Size(120, 225)
+        Me.ListBox1.TabIndex = 5
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(498, 26)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(112, 13)
+        Me.Label3.TabIndex = 6
+        Me.Label3.Text = "Canciones Guardadas"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.ListBox1)
+        Me.Controls.Add(Me.txtGrupo)
+        Me.Controls.Add(Me.txtTitulo)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.btnGuardarCancion)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents btnGuardarCancion As Button
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents txtTitulo As TextBox
+    Friend WithEvents txtGrupo As TextBox
+    Friend WithEvents ListBox1 As ListBox
+    Friend WithEvents Label3 As Label
+End Class
---
+++ 3ª Evaluacion/Repaso3ConexionFicheros/Form1.vb
@@ -0,0 +1,41 @@
+﻿Imports System.IO
+Public Class Form1
+    Dim canciones(99) As String
+    Dim ruta As String = Directory.GetCurrentDirectory() + "/canciones.txt"
+    Private Sub btnGuardarCancion_Click(sender As Object, e As EventArgs) Handles btnGuardarCancion.Click
+        Dim titulo As String = txtTitulo.Text
+        Dim grupo As String = txtGrupo.Text
+        Dim registro As String = titulo + "*" + grupo
+
+        Dim escritor As StreamWriter = New StreamWriter(ruta, True)
+        escritor.WriteLine(registro)
+
+        ListBox1.Items.Add(registro)
+        escritor.Close()
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        'Leer las canciones del fichero
+        Dim lector As StreamReader = New StreamReader(ruta)
+
+        Dim posicion As Integer = 0
+
+        While Not lector.EndOfStream
+            canciones(posicion) = lector.ReadLine()
+            posicion += 1
+        End While
+        lector.Close()
+
+        'volcar los titulos al listbox
+        For Each cancion As String In canciones
+            If Not cancion Is Nothing Then
+                ListBox1.Items.Add(cancion.Split("*")(0))
+            Else
+                Exit For
+            End If
+
+        Next
+    End Sub
+
+
+End Class
